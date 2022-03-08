435e5828f060e54bf13dc3875bfe731f1a752d77
Prince Arora <prince.arora@atos.net>
2022-03-08 12:25:05 +0530

    scss included

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './app.scss'
 
 export const Application = () => {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,75 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  output: {
-    // the output of the webpack build will be in /dist directory
-    path: path.resolve(__dirname, 'dist'),
-    // the filename of the JS bundle will be bundle.js
-    filename: 'bundle.js',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['ts-loader'],
-      },
-    ],
-  },
-  // add a custom index.html as the template
-  plugins: [
+const getPlugins = (argv) => {
+  const plugins = [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
-  ],
+  ]
+
+  if (argv.mode === 'production') {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      })
+    )
+  }
+  return plugins
+}
+
+const getDevConfig = () => {
+  return {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  }
+}
+
+module.exports = (env, argv) => {
+  const config = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ['ts-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            argv.mode !== 'production'
+              ? 'style-loader'
+              : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+  }
+  config.devServer = getDevConfig()
+  config.plugins = getPlugins(argv)
+
+  return config
 }
