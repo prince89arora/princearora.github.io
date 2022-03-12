bfbb5605cd78cc54d83104b8f067a1eab14a7f1a
Prince Arora <parora@pop-os.localdomain>
2022-03-13 00:27:52 +0530

    precommit and prettier added

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const getPlugins = (argv) => {
   const plugins = [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
-  ]
+  ];
 
-  if (argv.mode === 'production') {
+  if (argv.mode === "production") {
     plugins.push(
       new MiniCssExtractPlugin({
-        filename: '[name].css',
-        chunkFilename: '[id].css',
+        filename: "[name].css",
+        chunkFilename: "[id].css",
       })
-    )
+    );
   }
-  return plugins
-}
+  return plugins;
+};
 
 const getDevConfig = () => {
   return {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 9000,
-  }
-}
+  };
+};
+
+const getModules = (argv) => {
+  const rules = [];
+  rules.push({
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: ["babel-loader"],
+  });
+  rules.push({
+    test: /\.(ts|tsx)$/,
+    exclude: /node_modules/,
+    use: ["ts-loader"],
+  });
+  rules.push({
+    test: /\.s[ac]ss$/i,
+    use: [
+      argv.mode !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+      "css-loader",
+      "sass-loader",
+    ],
+  });
+
+  return { rules };
+};
 
 module.exports = (env, argv) => {
   const config = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
 
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
     },
 
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
     },
+  };
+  config.module = getModules();
+  config.devServer = getDevConfig();
+  config.plugins = getPlugins(argv);
 
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules/,
-          use: ['ts-loader'],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            argv.mode !== 'production'
-              ? 'style-loader'
-              : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-      ],
-    },
-  }
-  config.devServer = getDevConfig()
-  config.plugins = getPlugins(argv)
-
-  return config
-}
+  return config;
+};
